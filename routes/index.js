f4a606b86d2ce39ee41c2c2d4ee47ae9dde6dce9
jaehyeon2 <wol5971@gmail.com>
2021-12-17 22:12:22 +0900

    router index.js commit

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const Sequelize=require('sequelize');
+
+const {Comment, Hashtag, User, Video}=require('../models');
+const {isLoggedIn, isNotLoggedIn}=require('./middlewares');
+
+const router=express.Router();
+
+router.use((req, res, next)=>{
+    res.locals.user=req.user;
+    next();
+});
+
+router.get('/', async(req, res, next)=>{
+    try{
+        const videos=await Video.findAll({});
+        res.render('main', {title:'myTube', videos});
+    }catch(error){
+        console.error(error);
+        next(error);
+    }
+});
